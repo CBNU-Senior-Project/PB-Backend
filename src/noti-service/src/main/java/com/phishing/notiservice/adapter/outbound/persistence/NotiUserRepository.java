f738d59ab052a57e1b1d54470a521f875b694a2d
sso9594 <sso9594@naver.com>
2024-09-12 23:57:46 +0900

    feat: UserId 기준 NotiUser 검색 메서드 추가

--- src/noti-service/src/main/java/com/phishing/notiservice/adapter/outbound/persistence/NotiUserRepository.java
+++ src/noti-service/src/main/java/com/phishing/notiservice/adapter/outbound/persistence/NotiUserRepository.java
@@ -3,7 +3,9 @@ package com.phishing.notiservice.adapter.outbound.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface NotiUserRepository extends JpaRepository<NotiUserEntity, Long> {
+    Optional<NotiUserEntity> findByUserIdAndIsDeletedFalse(Long userId);
     List<NotiUserEntity> findByGroupIdAndIsDeletedFalse(Long groupId);
 }
