f0fab97dd292579529e1648c49dfac66e52293d4
sso9594 <sso9594@naver.com>
2024-11-11 22:18:02 +0900

    refactor: 정규식을 통한 예외처리

--- src/user-service/src/main/java/com/phishing/userservice/domain/group/payload/request/CreateGroupRequest.java
+++ src/user-service/src/main/java/com/phishing/userservice/domain/group/payload/request/CreateGroupRequest.java
@@ -1,6 +1,9 @@
 package com.phishing.userservice.domain.group.payload.request;
 
+import jakarta.validation.constraints.Pattern;
+
 public record CreateGroupRequest(
+        @Pattern(regexp = "^[a-zA-Z0-9가-힣]{1,15}$")
         String name
 
 ) {}
