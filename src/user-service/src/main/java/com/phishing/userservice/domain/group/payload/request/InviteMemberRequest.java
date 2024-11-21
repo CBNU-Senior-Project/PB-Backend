f10090471ad94902b77e329e95e596d9fc1d248a
sso9594 <sso9594@naver.com>
2024-11-21 23:46:08 +0900

    feat: grafana 설정

--- src/user-service/src/main/java/com/phishing/userservice/domain/group/payload/request/InviteMemberRequest.java
+++ src/user-service/src/main/java/com/phishing/userservice/domain/group/payload/request/InviteMemberRequest.java
@@ -3,7 +3,7 @@ package com.phishing.userservice.domain.group.payload.request;
 import jakarta.validation.constraints.Pattern;
 
 public record InviteMemberRequest(
-        @Pattern(regexp = "^01(?:0|1|[6-9])[.-]?(\\d{3}|\\d{4})[.-]?(\\d{4})$")
+        @Pattern(regexp = "^01(?:0|1|[6-9])\\d{3,4}\\d{4}$")
         String receiverPhoneNumber
 
 ) {}
